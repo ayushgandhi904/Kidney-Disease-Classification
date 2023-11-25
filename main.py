696e80c0ce74bcdf512d1ba83097febe1ec61426
Your Name <ayushgandhi904@gmail.com>
2023-11-26 00:34:46 +0530

    Data Ingestion step full completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.TumorClassifier import logger
+from TumorClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
-logger.info("Welcome to log file")
+STAGE_NAME = "Data Ingestion"
+try:
+    logger.info(f"---{STAGE_NAME} started---")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f"---{STAGE_NAME} completed ---")
+except Exception as e:
+    logger.exception(e)
+    raise e
