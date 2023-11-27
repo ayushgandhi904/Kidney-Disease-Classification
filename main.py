51f2d8c0ace8dda6c3000c73111453efbc548c09
Your Name <ayushgandhi904@gmail.com>
2023-11-28 00:02:17 +0530

    model trainer completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.TumorClassifier import logger
 from TumorClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from TumorClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from TumorClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion"
 try:
@@ -17,6 +18,17 @@ STAGE_NAME = "Prepare Base Model"
 try:
     logger.info(f"---{STAGE_NAME} started---")
     obj = PrepareBaseModelPipeline()
+    obj.main()
+    logger.info(f"---{STAGE_NAME} completed ---")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training"
+try:
+    logger.info(f"---{STAGE_NAME} started---")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f"---{STAGE_NAME} completed ---")
 except Exception as e:
---
+++ src/TumorClassifier/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,28 @@
+from TumorClassifier.config.configuration import ConfigurationManager
+from TumorClassifier.components.model_trainer import Training
+from TumorClassifier import logger
+
+STAGE_NAME = "Model Trainer"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config = training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+        
+if __name__ == "__main__":
+    try:
+        logger.info(f"---{STAGE_NAME} started---")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f"---{STAGE_NAME} completed ---")
+    
+    except Exception as e:
+        logger.exception(e)
+        raise e
